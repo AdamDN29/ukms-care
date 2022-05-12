a693a629f978f679a94cae635793d0f4646f4408
chandra19033 <67153739+chandra19033@users.noreply.github.com>
2022-05-12 19:03:45 +0700

    Update

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -7,22 +7,24 @@ import { ButtonGroup } from 'react-bootstrap'
 
 export default function BackButton () {
     return (
-        <div className='Frame323'>
-			<div className='Group384'>
-				<div className='Group385_1'>
-					<div className='Group239_1'>
-						<div className='Group294_1'>
-							<div className='Group293_1'>
-								<div className='Rectangle13_1'/>
+		<div className='grup1'>
+        	<div className='Frame323'>
+				<div className='Group384'>
+					<div className='Group385_1'>
+						<div className='Group239_1'>
+							<div className='Group294_1'>
+								<div className='Group293_1'>
+									<div className='Rectangle13_1'/>
+								</div>
 							</div>
 						</div>
+						<ButtonGroup className='Group322_1'>
+							<div className='Group321_1'>
+								<span className='Kembali_1'>Kembali</span>
+							</div>
+							<img className='Vector_6' src = {ImgAsset.BeritaSingle_Vector_6} />
+						</ButtonGroup>
 					</div>
-					<ButtonGroup className='Group322_1'>
-						<div className='Group321_1'>
-							<span className='Kembali_1'>Kembali</span>
-						</div>
-						<img className='Vector_6' src = {ImgAsset.BeritaSingle_Vector_6} />
-					</ButtonGroup>
 				</div>
 			</div>
 		</div>
